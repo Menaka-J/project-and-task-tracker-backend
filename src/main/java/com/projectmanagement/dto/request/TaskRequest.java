754f4136481b65d4853ad097928a2e6bdec5fdf1
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:53:54 +0530

    commit

--- src/main/java/com/projectmanagement/dto/request/TaskRequest.java
+++ src/main/java/com/projectmanagement/dto/request/TaskRequest.java
@@ -1,4 +1,19 @@
 package com.projectmanagement.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class TaskRequest {
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @NotNull
+    private Long assigneeId;
+
+    private LocalDate deadline;
 }
