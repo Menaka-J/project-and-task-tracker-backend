52b266c31abb21a8544d35d8f8d6727bf0078d0b
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:55:23 +0530

    commit

--- src/main/java/com/projectmanagement/dto/response/ProjectResponse.java
+++ src/main/java/com/projectmanagement/dto/response/ProjectResponse.java
@@ -1,4 +1,23 @@
 package com.projectmanagement.dto.response;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class ProjectResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private UserInfo createdBy;
+    private List<UserInfo> members;
+    private Integer totalTasks;
+
+    @Data
+    public static class UserInfo {
+        private Long id;
+        private String name;
+        private String email;
+    }
 }
