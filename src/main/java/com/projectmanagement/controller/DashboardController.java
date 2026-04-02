5ba7a5ef2505ba11295145a1c79c0ed6d1e27c8b
Menaka J <menakaraman4@gmail.com>
2026-04-02 23:03:00 +0530

    commit

--- src/main/java/com/projectmanagement/controller/DashboardController.java
+++ src/main/java/com/projectmanagement/controller/DashboardController.java
@@ -1,4 +1,21 @@
 package com.projectmanagement.controller;
 
+import com.projectmanagement.dto.response.DashboardResponse;
+import com.projectmanagement.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/dashboard")
 public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<DashboardResponse> getDashboardStats() {
+        return ResponseEntity.ok(dashboardService.getDashboardStats());
+    }
 }
