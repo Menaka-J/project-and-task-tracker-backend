cd390294d3c0beaf08aeb0f76ca3dae2e1ef6720
Menaka J <menakaraman4@gmail.com>
2026-04-02 23:01:26 +0530

    commit

--- src/main/java/com/projectmanagement/controller/AuthController.java
+++ src/main/java/com/projectmanagement/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.projectmanagement.controller;
 
+import com.projectmanagement.dto.request.LoginRequest;
+import com.projectmanagement.dto.request.RegisterRequest;
+import com.projectmanagement.dto.response.AuthResponse;
+import com.projectmanagement.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-}
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+        try {
+            AuthResponse response = authService.register(request
