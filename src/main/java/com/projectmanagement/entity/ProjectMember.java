84d211fc6724ae62b881a1cedaaba85d46f52634
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:50:48 +0530

    commit

--- src/main/java/com/projectmanagement/entity/ProjectMember.java
+++ src/main/java/com/projectmanagement/entity/ProjectMember.java
@@ -1,4 +1,35 @@
 package com.projectmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project_members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "added_at")
+    private LocalDateTime addedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        addedAt = LocalDateTime.now();
+    }
 }
