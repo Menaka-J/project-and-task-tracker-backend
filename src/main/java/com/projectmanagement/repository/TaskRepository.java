cec265e783c93ec88e23be4d038b75710fc82e8b
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:52:04 +0530

    commit

--- src/main/java/com/projectmanagement/repository/TaskRepository.java
+++ src/main/java/com/projectmanagement/repository/TaskRepository.java
@@ -1,4 +1,15 @@
 package com.projectmanagement.repository;
 
-public class TaskRepository {
+import com.projectmanagement.entity.Task;
+import com.projectmanagement.entity.Project;
+import com.projectmanagement.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProject(Project project);
+    List<Task> findByProjectAndStatus(Project project, TaskStatus status);
+    long countByStatus(TaskStatus status);
 }
