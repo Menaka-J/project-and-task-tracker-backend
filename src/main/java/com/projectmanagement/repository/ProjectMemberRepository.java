e6c3e2fbefe1ba95b1fe8811ab58a97a1c9a9638
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:52:30 +0530

    commit

--- src/main/java/com/projectmanagement/repository/ProjectMemberRepository.java
+++ src/main/java/com/projectmanagement/repository/ProjectMemberRepository.java
@@ -1,4 +1,14 @@
 package com.projectmanagement.repository;
 
-public class ProjectMemberRepository {
+import com.projectmanagement.entity.ProjectMember;
+import com.projectmanagement.entity.Project;
+import com.projectmanagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+    boolean existsByProjectAndUser(Project project, User user);
+    Optional<ProjectMember> findByProjectAndUser(Project project, User user);
 }
