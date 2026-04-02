56b2f8aaf88cc0604c62244e7351998ffd48d1ff
Menaka J <menakaraman4@gmail.com>
2026-04-02 22:56:35 +0530

    commit

--- src/main/java/com/projectmanagement/dto/response/UserResponse.java
+++ src/main/java/com/projectmanagement/dto/response/UserResponse.java
@@ -1,4 +1,13 @@
 package com.projectmanagement.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
 }
